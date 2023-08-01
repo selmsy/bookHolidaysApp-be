4fc0e8f3cf1db248a11578adf479e692953b6f6a
Selma Omanovic <selma.om1@gmail.com>
2023-08-01 12:38:55 +0200

    Adding database configuration

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
 
-dotenv.config()
+dotenv.config();
+
+const connect = async() => {
+    try{
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to database!");
+    } catch (error) {
+        throw error;
+    }
+
+};
+
+mongoose.connection.on("disconnected", ()=>{
+    console.log("Database disconnected!");
+})
+mongoose.connection.on("connected", ()=>{
+    console.log("Database connected!");
+})
 
 app.listen(8800, ()=>{
+    connect()
     console.log("Connection established!");
 })
